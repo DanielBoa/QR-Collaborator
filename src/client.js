94e0210535c1e6e5f9dbe9b0d7401169f6b3d75d
Daniel Boa <daniel.boa@gmail.com>
2018-09-12 20:39:54 +0100

    Added concept of piece and have it rendering. Changed abstraction of grid to pixel relationship a bit.

--- src/client.js
+++ src/client.js
@@ -3,13 +3,41 @@ const ctx = canvas.getContext('2d');
 const gridWidth = 29;
 const gridHeight = 29;
 let scale = null;
-const blockSize = 1;
+const blockSize = 10000;
 const pieces = [];
 
-function updateSizes() {
+class Piece {
+  constructor(shape) {
+    this.shape = shape;
+    this.pos = { x: 0, y: 0 };
+  }
+
+  draw() {
+    const { shape, pos } = this;
+
+    ctx.save();
+    ctx.translate(c(pos.x), c(pos.y)); 
+
+    for (let y = 0; y < shape.length; y++) {
+      row = shape[y];
+
+      for (let x = 0; x < row.length; x++) {
+        ctx.fillStyle = `rgba(${(row[x] === 1) ? '0,0,0' : '255,255,255'}, 0.9)`;
+        ctx.fillRect(c(x), c(y), c(), c());
+      }
+    }
+
+    ctx.restore();
+  }
+}
+
+// convert position to pixel value
+const c = (position = 1) => position * blockSize;
+
+function resizeHandler() {
   const { innerWidth: width, innerHeight: height } = window;
   
-  scale = width > height ? (height / gridHeight) : (width / gridWidth);
+  scale = width > height ? (height / c(gridHeight)) : (width / c(gridWidth));
 
   canvas.width = width;
   canvas.height = height;
@@ -17,20 +45,21 @@ function updateSizes() {
   ctx.scale(scale, scale);
 }
 
-function drawBoard() {
+function clearCanvas() {
   const { width, height } = canvas;
-  const xOffset = ((width / scale) - gridWidth) / 2;
-  const yOffset = ((height / scale) - gridHeight) / 2;
 
+  ctx.clearRect(0, 0, c(width), c(height));
+}
+
+function drawBoard() {
   ctx.save();
-  ctx.translate(xOffset, yOffset);
 
   for (let y = 0; y <= gridHeight; y++) {
     for (let x = 0; x <= gridWidth; x++) {
-      const opacity = (((y * gridWidth) + x) % 2) ? 0.2 : 0.5;
+      const opacity = (((y * gridWidth) + x) % 2) ? 0.1 : 0.2;
 
       ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
-      ctx.fillRect(x, y, blockSize, blockSize);
+      ctx.fillRect(c(x), c(y), c(), c());
     }
   }
 
@@ -38,19 +67,42 @@ function drawBoard() {
 }
 
 function draw() {
-  const { width, height } = canvas;
+  clearCanvas();
 
-  ctx.clearRect(0, 0, width, height);
+  const { width, height } = canvas;
+  const xOffset = ((width / scale) - c(gridWidth)) / 2;
+  const yOffset = ((height / scale) - c(gridHeight)) / 2;
+
+  ctx.save();
+  ctx.translate(xOffset, yOffset);
 
   drawBoard();
 
-  requestAnimationFrame(draw);
+  for (let piece of pieces) {
+    piece.draw();
+  }
+
+  ctx.restore();
+
+  requestAnimationFrame(draw); 
 }
 
 function setup() {
-  updateSizes();
+  pieces.push(
+    new Piece([
+      [1,0,1,1,0,1,1],
+      [1,1,0,1,1,0,0],
+      [1,0,1,0,1,1,1],
+      [1,1,0,1,0,1,1],
+      [0,0,0,1,0,0,0],
+      [1,1,0,0,0,0,0],
+      [0,1,0,0,0,0,0],
+    ]),
+  );
 
-  window.addEventListener('resize', updateSizes);
+  resizeHandler();
+
+  window.addEventListener('resize', resizeHandler);
 
   requestAnimationFrame(draw);
 }
